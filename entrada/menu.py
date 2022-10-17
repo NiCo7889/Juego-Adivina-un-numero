b7e6f1a29461a6d002449470d2de4f03c8cd031f
NiCo7889 <113980882+NiCo7889@users.noreply.github.com>
2022-10-18 00:18:05 +0200

    commit final

---
+++ entrada/menu.py
@@ -0,0 +1,19 @@
+#niveles de dificultad
+niveles = [sencillo, intermedio, avanzado, experto]
+print(niveles)
+nivel_de_dificultad = Input("Escoja un nivel de dificultad: ")
+sencillo = (0,100)
+intermedio = (0, 1000)
+avanzado = (0, 1000000)
+experto = (0, 1000000000000)
+if nivel == sencillo:
+    print("Ha escogido el nivel sencillo. ")
+elif nivel == intermedio:
+     print("Ha escogido el nivel intermedio. ")
+elif nivel == avanzado:
+        print("Ha escogido el nivel avanzado. ")
+elif nivel == experto:
+        print("Ha escogido el nivel experto. ")
+
+
+    
--- lanzador.py
+++ lanzador.py
@@ -1,9 +1,4 @@
-"""
-Módulo que sirve únicamente para iniciar el juego
-"""
-
 from juego import jugar
 
 if __name__ == "__main__":
     jugar()
-
